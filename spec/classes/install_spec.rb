e9eff47c7b10a886e64fc2cf8c3f1eeab02efa82
Geert Theys <geert.theys@gmail.com>
2015-07-20 14:43:29 +0700

    non working tests

--- spec/classes/install_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'couchbase_sync_gateway::install' do
-
-  context 'install couchbase_sync_gateway' do
-    it { should contain_package('couchbase-sync-gateway')}
-  end
-end
